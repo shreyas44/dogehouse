0043b5fb924400bfea9dad9dc1cf62689c31d5cc
Shreyas Sreenivas <46835608+shreyas44@users.noreply.github.com>
2021-03-13 19:51:14 +0530

    Add meta description tag

--- kibbeh/src/pages/_app.tsx
+++ kibbeh/src/pages/_app.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 import "../styles/globals.css";
 import { AppProps } from "next/app";
+import { Head } from "next/head";
 
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta name="description" content="Taking voice conversations to the moon" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default App;
